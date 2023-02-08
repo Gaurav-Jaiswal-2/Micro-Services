0bbef32da4971cb5319a78545995878f8d40b0ac
Gaurav <aryanjaiswal5928@gmail.com>
2023-02-08 19:13:54 +0530

    added the config server

--- UsersService/src/main/java/com/keyo/controllers/UsersController.java
+++ UsersService/src/main/java/com/keyo/controllers/UsersController.java
@@ -2,6 +2,7 @@ package com.keyo.controllers;
 
 import java.util.List;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,10 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.keyo.entities.Users;
 import com.keyo.service.UsersServiceimpl;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping("users/")
 public class UsersController {
 
+	private org.slf4j.Logger logger = LoggerFactory.getLogger(UsersController.class);
+	
 	@Autowired
 	private UsersServiceimpl userService;
 	
@@ -28,10 +33,19 @@ public class UsersController {
 	          return new ResponseEntity<>(message, HttpStatus.CREATED);
 	}
 	
+	@CircuitBreaker(name = "RatingHotelBreaker", fallbackMethod = "RatingHotelFallback")
 	@GetMapping("user/{userId}")
 	ResponseEntity<Users> getUserByUserId(@PathVariable String userId){
 		    Users users = this.userService.getUsers(userId);
 		    return new ResponseEntity<>(users, HttpStatus.ACCEPTED);
+	}
+	
+	public ResponseEntity<Users> ratingHotelFallback(String userId, Exception ex){
+	logger.info("Fallback executed because service is down" , ex.getMessage());
+	
+	//if you are creating user with build method use @builder on the user class
+	Users users = new Users("000", "dummy", "this is a dummy user because service is down", null);
+	return new ResponseEntity<>(users,HttpStatus.OK);
 	}
 	
 	@GetMapping("all-users")
--- UsersService/src/main/java/com/keyo/entities/Users.java
+++ UsersService/src/main/java/com/keyo/entities/Users.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,6 +16,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Builder
 public class Users {
 	@Id
 	private String userId;
